f467d3887b52785e9515cb979c2f9edaf2578e36
nikitapoly <nikitapoly@comcast.net>
2022-11-05 20:12:46 -0700

    built resume page

--- front-end/src/routes/resume/index@home.tsx
+++ front-end/src/routes/resume/index@home.tsx
@@ -1,8 +1,25 @@
-import { component$ } from "@builder.io/qwik";
+import { component$, useStylesScoped$ } from "@builder.io/qwik";
 import type { DocumentHead } from "@builder.io/qwik-city";
+import styles from "./index.css";
 
 export default component$(() => {
-  return <main class="home"></main>;
+  useStylesScoped$(styles);
+  return (
+    <main class="resume">
+      <ul>
+        <button class="github">GitHub</button>
+        <button class="linkedin">LinkedIn</button>
+        <button class="resume">Resume</button>
+      </ul>
+      <p>
+        jlksdhf sdlfh sdkfh sdkf sdfh sdk fksdhfjksdhfkjsd lsdh lksdhf sdlkfh sdlkfh sd fhsdlfsdlkfhl sdfh
+        sdlkf hsdlkfh sdlkfh sdlfh sdlkfh sdl fhlksdfh lksdfh sdlkfh sdlkf jlksdhf sdlfh sdkfh sdkf sdfh sdk
+        fksdhfjksdhfkjsd lsdh lksdhf sdlkfh sdlkfh sd fhsdlfsdlkfhl sdfh sdlkf hsdlkfh sdlkfh sdlfh sdlkfh sdl
+        fhlksdfh lksdfh sdlkfh sdlkf jlksdhf sdlfh sdkfh sdkf sdfh sdk fksdhfjksdhfkjsd lsdh lksdhf sdlkfh
+        sdlkfh sd fhsdlfsdlkfhl sdfh sdlkf hsdlkfh sdlkfh sdlfh sdlkfh sdl fhlksdfh lksdfh sdlkfh sdlkf
+      </p>
+    </main>
+  );
 });
 export const head: DocumentHead = {
   title: "Resume",
