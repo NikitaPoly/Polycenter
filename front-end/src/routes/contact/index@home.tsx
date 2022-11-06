72a1c51d2f1f5475c16d4c15da30adba030893f8
nikitapoly <nikitapoly@comcast.net>
2022-11-05 21:53:46 -0700

    basic for all home pages complete

--- front-end/src/routes/contact/index@home.tsx
+++ front-end/src/routes/contact/index@home.tsx
@@ -1,8 +1,27 @@
-import { component$ } from "@builder.io/qwik";
+import { component$, useStylesScoped$ } from "@builder.io/qwik";
 import type { DocumentHead } from "@builder.io/qwik-city";
 
+import styles from "./index.css";
+
 export default component$(() => {
-  return <main class="home"></main>;
+  useStylesScoped$(styles);
+  return (
+    <main class="contact">
+      <label class="email">
+        Email <input type="email" />
+      </label>
+      <label class="title">
+        Title <input type="text" />
+      </label>
+      <label class="body">
+        Body <textarea />
+      </label>
+      <div class="button">
+        <button>Send</button>
+      </div>
+      <div class="error">Error message</div>
+    </main>
+  );
 });
 export const head: DocumentHead = {
   title: "Contact",
