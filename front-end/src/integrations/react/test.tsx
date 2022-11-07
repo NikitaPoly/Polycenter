4868f0bdde32913611d3de9987df8e073c6cc7dd
nikitapoly <nikitapoly@comcast.net>
2022-11-06 22:26:21 -0800

    removed qwik

--- front-end/src/integrations/react/test.tsx
+++
@@ -1,5 +0,0 @@
-import { component$ } from "@builder.io/qwik";
-
-export default component$(() => {
-  return <div>Three.js but rn sucks</div>;
-});
--- front-end/src/routes/three/index.tsx
+++
@@ -1,6 +0,0 @@
-import { component$ } from "@builder.io/qwik";
-import Test from "../../integrations/react/test";
-
-export default component$(() => {
-  return <Test />;
-});
