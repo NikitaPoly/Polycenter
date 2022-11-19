509ac3af92e5851613ae5641092c805163cc91fc
nikitapoly <nikitapoly@comcast.net>
2022-11-19 15:25:55 -0800

    testing on linux

--- next-front-end/pages/sds/index.tsx
+++ next-front-end/pages/sds/index.tsx
@@ -3,7 +3,6 @@ import { useSession, signIn, signOut } from "next-auth/react";
 
 export default function SDShome() {
   const { data: session } = useSession();
-  console.log(session);
   return (
     <Layout>
       <main className="sds">
--- next-front-end/pages/sds/order.tsx
+++ next-front-end/pages/sds/order.tsx
@@ -2,9 +2,10 @@ import Layout from "../../layouts/sds";
 import { useSession, getSession } from "next-auth/react";
 
 export default function SDShome() {
+  const { data: session } = useSession();
   return (
     <Layout>
-      <main className="sds"></main>
+      <main className="sds">{`Welcome ${((session as any).user as any).name}`}</main>
     </Layout>
   );
 }
